031587e0bc0f244f1180c9ebe52cae88df5b34e3
jf99 <jf99@github.com>
2023-08-15 14:31:04 +0200

    unlink eigen

--- build.sh
+++ build.sh
@@ -1,24 +1,25 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
+"$SCRIPT_DIR/unlink-eigen.sh"
 make -j4
 
 cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
-
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
+"$SCRIPT_DIR/unlink-eigen.sh"
 make -j4
 
 cd ../../../
 
 echo "Configuring and building ORB_SLAM2 ..."
-
 cd Vocabulary
 tar -xjf voc.tar.bz2
 cd ..
@@ -26,4 +27,5 @@ cd ..
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DTORCH_PATH=/home/t/Workspace/deps/pytorch/torch/share/cmake/Torch
+"$SCRIPT_DIR/unlink-eigen.sh"
 make -j4
